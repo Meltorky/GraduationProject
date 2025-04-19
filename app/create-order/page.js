80d5551b36fddb8263b81144c15677bfe51b01c7
Mohamed Eltorky <mohamed.eltorky1155@gmail.com>
2025-04-20 00:10:42 +0200

    add Suspense in create order

--- app/create-order/page.js
+++ app/create-order/page.js
@@ -1,7 +1,7 @@
 // /create-order/page.js
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import styles from "./CreateOrder.module.css";
 import { getToken } from "/lib/auth";
@@ -151,133 +151,135 @@ export default function CreateOrderPage() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.header}>
-        <h1 className={styles.title}>Complete Your Order</h1>
-        <button className={styles.goback} onClick={goBack}>
-          Cancel
-        </button>
-      </div>
-      {error && <div className={styles.errorMessage}>{error}</div>}
-
-      {success ? (
-        <div className={styles.successMessage}>
-          Your order has been successfully created! Redirecting...
-          <PaymentHandler orderNumber={orderNumber} />
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Complete Your Order</h1>
+          <button className={styles.goback} onClick={goBack}>
+            Cancel
+          </button>
         </div>
-      ) : (
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <div className={styles.formGroup}>
-            <label className={styles.label}>Order Summary</label>
-            <div className={styles.orderSummary}>
-              <div className={styles.summaryRow}>
-                <span>Cart ID:</span>
-                <input
-                  type="text"
-                  value={formData.cartId}
-                  disabled
-                  className={styles.disabledInput}
-                />
-              </div>
-              {cartTotal && (
+        {error && <div className={styles.errorMessage}>{error}</div>}
+
+        {success ? (
+          <div className={styles.successMessage}>
+            Your order has been successfully created! Redirecting...
+            <PaymentHandler orderNumber={orderNumber} />
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Order Summary</label>
+              <div className={styles.orderSummary}>
                 <div className={styles.summaryRow}>
-                  <span>Total Amount:</span>
+                  <span>Cart ID:</span>
                   <input
                     type="text"
-                    value={`EGP ${cartTotal}`}
+                    value={formData.cartId}
                     disabled
                     className={styles.disabledInput}
                   />
                 </div>
-              )}
+                {cartTotal && (
+                  <div className={styles.summaryRow}>
+                    <span>Total Amount:</span>
+                    <input
+                      type="text"
+                      value={`EGP ${cartTotal}`}
+                      disabled
+                      className={styles.disabledInput}
+                    />
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.formGroup}>
-            <label className={styles.label}>Shipping Method</label>
-            <select
-              name="shippingDTO.shippingMethod"
-              value={formData.shippingDTO.shippingMethod}
-              onChange={handleChange}
-              className={styles.select}
-              required
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Shipping Method</label>
+              <select
+                name="shippingDTO.shippingMethod"
+                value={formData.shippingDTO.shippingMethod}
+                onChange={handleChange}
+                className={styles.select}
+                required
+              >
+                <option value="Standard">Standard</option>
+                <option value="Express">Express</option>
+              </select>
+              <div style={{ marginBlock: "10px" }}>
+                <h3 style={{ color: "var(--accent)", fontSize: ".9rem" }}>
+                  For Express : Delivery after 4 days for EGP 20
+                </h3>
+                <h3 style={{ color: "var(--accent)", fontSize: ".9rem" }}>
+                  For Standard : Delivery after 2 days for EGP 40
+                </h3>
+              </div>
+            </div>
+
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Address</label>
+              <input
+                type="text"
+                name="shippingDTO.address"
+                value={formData.shippingDTO.address}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder="Street Address"
+                required
+              />
+            </div>
+
+            <div className={styles.formRow}>
+              <div className={styles.formGroup}>
+                <label className={styles.label}>City</label>
+                <input
+                  type="text"
+                  name="shippingDTO.city"
+                  value={formData.shippingDTO.city}
+                  onChange={handleChange}
+                  className={styles.input}
+                  placeholder="City"
+                  required
+                />
+              </div>
+
+              <div className={styles.formGroup}>
+                <label className={styles.label}>Postal Code</label>
+                <input
+                  type="text"
+                  name="shippingDTO.postalCode"
+                  value={formData.shippingDTO.postalCode}
+                  onChange={handleChange}
+                  className={styles.input}
+                  placeholder="Postal Code"
+                  required
+                />
+              </div>
+            </div>
+
+            <div className={styles.formGroup}>
+              <label className={styles.label}>Country</label>
+              <input
+                type="text"
+                name="shippingDTO.country"
+                value={formData.shippingDTO.country}
+                onChange={handleChange}
+                className={styles.input}
+                placeholder="Country"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className={styles.submitButton}
+              disabled={loading}
             >
-              <option value="Standard">Standard</option>
-              <option value="Express">Express</option>
-            </select>
-            <div style={{ marginBlock: "10px" }}>
-              <h3 style={{ color: "var(--accent)", fontSize: ".9rem" }}>
-                For Express : Delivery after 4 days for EGP 20
-              </h3>
-              <h3 style={{ color: "var(--accent)", fontSize: ".9rem" }}>
-                For Standard : Delivery after 2 days for EGP 40
-              </h3>
-            </div>
-          </div>
-
-          <div className={styles.formGroup}>
-            <label className={styles.label}>Address</label>
-            <input
-              type="text"
-              name="shippingDTO.address"
-              value={formData.shippingDTO.address}
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Street Address"
-              required
-            />
-          </div>
-
-          <div className={styles.formRow}>
-            <div className={styles.formGroup}>
-              <label className={styles.label}>City</label>
-              <input
-                type="text"
-                name="shippingDTO.city"
-                value={formData.shippingDTO.city}
-                onChange={handleChange}
-                className={styles.input}
-                placeholder="City"
-                required
-              />
-            </div>
-
-            <div className={styles.formGroup}>
-              <label className={styles.label}>Postal Code</label>
-              <input
-                type="text"
-                name="shippingDTO.postalCode"
-                value={formData.shippingDTO.postalCode}
-                onChange={handleChange}
-                className={styles.input}
-                placeholder="Postal Code"
-                required
-              />
-            </div>
-          </div>
-
-          <div className={styles.formGroup}>
-            <label className={styles.label}>Country</label>
-            <input
-              type="text"
-              name="shippingDTO.country"
-              value={formData.shippingDTO.country}
-              onChange={handleChange}
-              className={styles.input}
-              placeholder="Country"
-              required
-            />
-          </div>
-
-          <button
-            type="submit"
-            className={styles.submitButton}
-            disabled={loading}
-          >
-            {loading ? "Processing..." : "Complete Order"}
-          </button>
-        </form>
-      )}
-    </div>
+              {loading ? "Processing..." : "Complete Order"}
+            </button>
+          </form>
+        )}
+      </div>
+    </Suspense>
   );
 }
